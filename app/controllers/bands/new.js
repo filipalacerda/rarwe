e111f37e22560277e9dedc1c16ff3fbb6fc5b836
Filipa Lacerda <filipalacerda@users.noreply.github.com>
2024-09-17 17:05:50 +0100

    Fetch API

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -1,9 +1,9 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { dasherize } from '@ember/string';
 import { tracked } from '@glimmer/tracking';
 import Band from 'rarwe/models/band';
 import { service } from '@ember/service';
+import fetch from 'fetch';
 
 export default class BandsNewController extends Controller {
   @service catalog;
@@ -21,9 +21,27 @@ export default class BandsNewController extends Controller {
   }
 
   @action
-  saveBand() {
-    const band = new Band({ name: this.name, id: dasherize(this.name) });
-    this.catalog.add('band', band);
-    this.router.transitionTo('bands.band.songs', band.id);
+  async saveBand() {
+    const response = await fetch('/bands', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+      },
+      body: JSON.stringify({
+        type: 'bands',
+        attributes: {
+          name: this.name,
+        },
+      }),
+    });
+
+    const json = await response.json();
+
+    const { id, attributes } = json.data;
+
+    const record = new Band({ id, ...attributes });
+
+    this.catalog.add('band', record);
+    this.router.transitionTo('bands.band.songs', id);
   }
 }
--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,66 +1,26 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-import Band from '../models/band';
-//import Band from 'rarwe/models/band';
-import Song from '../models/song';
+import Band from 'rarwe/models/band';
+import fetch from 'fetch';
 
 export default class BandsRoute extends Route {
   @service catalog;
 
-  model() {
-    const blackDog = new Song({
-      title: 'Black Dog',
-      rating: 3,
+  async model() {
+    let json = await fetch('/bands').then((response) => {
+      return response.json();
     });
 
-    const yellowLedbetter = new Song({
-      title: 'Yellow Ledbetter',
-      rating: 4,
-    });
+    for (let item of json.data) {
+      let { id, attributes, relationships } = item;
+      let rels = {};
+      for (let relationshipName in relationships) {
+        rels[relationshipName] = relationships[relationshipName].links.related;
+      }
+      let record = new Band({ id, ...attributes }, rels);
 
-    const pretender = new Song({
-      title: 'The Pretender',
-      rating: 2,
-    });
-
-    const daughter = new Song({
-      title: 'Daughter',
-      band: 'Pearl Jam',
-      rating: 5,
-    });
-
-    const ledZeppelin = new Band({
-      id: 'led-zeppelin',
-      name: 'Led Zeppelin',
-      songs: [blackDog],
-    });
-
-    const pearlJam = new Band({
-      id: 'pearl-jam',
-      name: 'Pearl Jam',
-      songs: [yellowLedbetter, daughter],
-    });
-
-    const fooFighters = new Band({
-      id: 'foo-fighters',
-      name: 'Foo Fighters',
-      songs: [pretender],
-    });
-
-    blackDog.band = ledZeppelin;
-    yellowLedbetter.band = pearlJam;
-    daughter.band = pearlJam;
-    pretender.band = fooFighters;
-
-    this.catalog.add('song', blackDog);
-    this.catalog.add('song', yellowLedbetter);
-    this.catalog.add('song', daughter);
-    this.catalog.add('song', pretender);
-
-    this.catalog.add('band', ledZeppelin);
-    this.catalog.add('band', pearlJam);
-    this.catalog.add('band', fooFighters);
-
+      this.catalog.add('band', record);
+    }
     return this.catalog.bands;
   }
 }
