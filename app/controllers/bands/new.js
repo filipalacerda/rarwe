8ed8ce5a9fbfc35d3408feacf001f328ff8d2492
Filipa Lacerda <filipalacerda@users.noreply.github.com>
2024-09-17 13:27:28 +0100

    Fix new band

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -1,7 +1,22 @@
 import Controller from '@ember/controller';
-
+import { action } from '@ember/object';
+import { dasherize } from '@ember/string';
+import { tracked } from '@glimmer/tracking';
+import { Band } from 'rarwe/routes/bands';
 export default class BandsNewController extends Controller {
-  resetController(controller) {
-    controller.name = '';
+  @tracked name;
+
+  get hasNoName() {
+    return !this.name;
+  }
+
+  @action
+  updateName(event) {
+    this.name = event.target.value;
+  }
+
+  @action
+  saveBand() {
+    new Band({ name: this.name, id: dasherize(this.name) });
   }
 }
--- app/routes/bands/new.js
+++ app/routes/bands/new.js
@@ -1,23 +1,7 @@
 import Route from '@ember/routing/route';
-import { action } from '@ember/object';
-import { dasherize } from '@ember/string';
-import { tracked } from '@glimmer/tracking';
-import { Band } from 'rarwe/routes/bands';
 
 export default class BandsNewRoute extends Route {
-  @tracked name;
-
-  get hasNoName() {
-    return !this.name;
-  }
-
-  @action
-  updateName(event) {
-    this.name = event.target.value;
-  }
-
-  @action
-  saveBand() {
-    new Band({ name: this.name, id: dasherize(this.name) });
+  resetController(controller) {
+    controller.name = '';
   }
 }
