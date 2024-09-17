238ff6a16c7aa1e26fd951c03f35d50dc9c347c1
Filipa Lacerda <filipalacerda@users.noreply.github.com>
2024-09-17 13:18:51 +0100

    Reset controller

--- app/routes/bands/songs.js
+++ app/routes/bands/songs.js
@@ -1,3 +1,8 @@
 import Route from '@ember/routing/route';
 
-export default class BandsBandSongsRoute extends Route {}
+export default class BandsBandSongsRoute extends Route {
+  resetController(controller) {
+    controller.title = '';
+    controller.showAddSong = true;
+  }
+}
