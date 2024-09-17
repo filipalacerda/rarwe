0e84d7b1c1cd9a47a1ff4215bc9155b8ffd2db5a
Filipa Lacerda <filipalacerda@users.noreply.github.com>
2024-09-17 12:40:20 +0100

    Nested templates

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class BandsBandRoute extends Route {
   model(params) {
-    let bands = this.modelFor('bands');
+    const bands = this.modelFor('bands');
     return bands.find((band) => band.id === params.id);
   }
 }
