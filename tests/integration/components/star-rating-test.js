ac33c59e6ff35c0b1d35049751b81647ec14eefc
Filipa Lacerda <filipalacerda@users.noreply.github.com>
2024-09-18 11:50:41 +0100

    Integration tests

--- tests/integration/components/star-rating-test.js
+++ tests/integration/components/star-rating-test.js
@@ -1,26 +1,55 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'rarwe/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | star-rating', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set('rating', 4);
+    this.set('updateRating', () => {});
 
-    await render(hbs`<StarRating />`);
+    await render(
+      hbs`<StarRating @rating={{this.rating}} @onUpdate={{this.updateRating}} />`,
+    );
 
-    assert.dom().hasText('');
+    assert
+      .dom('[data-test-rr="full-star"]')
+      .exists({ count: 4 }, 'The rihjt amount of full stars is rendered');
 
-    // Template block usage:
-    await render(hbs`
-      <StarRating>
-        template block text
-      </StarRating>
-    `);
+    assert
+      .dom('[data-test-rr="empty-star"]')
+      .exists({ count: 1 }, 'The right amount of empty stars is rendered');
 
-    assert.dom().hasText('template block text');
+    this.set('rating', 2);
+
+    assert
+      .dom('[data-test-rr="full-star"]')
+      .exists(
+        { count: 2 },
+        'The right amount of full stars is rendered after changing rating',
+      );
+
+    assert
+      .dom('[data-test-rr="empty-star"]')
+      .exists(
+        { count: 3 },
+        'The right amount of empty stars is rendered after changing rating',
+      );
+  });
+
+  test('Calls onUpdate with the correct value', async function (assert) {
+    this.set('rating', 2);
+    this.set('updateRating', (rating) => {
+      assert.step(`Updated to rating: ${rating}`);
+    });
+
+    await render(
+      hbs`<StarRating @rating={{this.rating}} @onUpdate={{this.updateRating}} />`,
+    );
+
+    await click('[data-test-rr="star-rating-button"]:nth-child(4)');
+    assert.verifySteps(['Updated to rating: 4']);
   });
 });
